d6b801b295b844aef2079d05a5de0ac05bb440eb
cbtnt2 <80375713+cbtnt2@users.noreply.github.com>
2022-07-15 09:44:49 +1200

    added contents for header and body

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('kale, Spinach & rocket smoothie')
+streamlit.text('Hard-boiled free-range egg')
